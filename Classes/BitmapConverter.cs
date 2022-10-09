110e3594f0a7918caa40bc4d68f60a556c2b2bd2
Dmitry Bogov <dimasovch@yandex.ru>
2022-10-09 19:58:57 +0500

    Добавьте файлы проекта.

---
+++ Classes/BitmapConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace BitmapConvrerting
+{
+    static class BitmapConverter
+    {
+        
+        static public Bitmap ConvertToBitmap(double[,] data, Func<double, Color> DoubleToColorFunc) 
+        {
+
+            if (data.GetLength(0) != data.GetLength(1))
+            {
+                throw new Exception("Array is not the correct size");
+            }
+            int size = data.GetLength(0);
+
+            Bitmap result = new Bitmap(size, size);
+
+            for (int x = 0; x < size; x++ )
+                for (int y = 0; y < size; y++ )
+                {
+                    result.SetPixel(x, y, DoubleToColorFunc(data[x, y]));
+                }
+            return result;
+        }
+        static public void SaveBitmap(Bitmap image, string fileName)
+        {
+            image.Save(fileName);
+        }
+  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Terrain;
+using OpenSimplex;
+using BitmapConvrerting;
+using System.Drawing;
+
+namespace IslandGenerator
+{
+
+    class NoiseGeneratorWrapper: OpenSimplexNoise, INoiseGenerator
+    {
+        public NoiseGeneratorWrapper(long seed) : base(seed) { }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long seed = 0;
+            int size = 128;
+            double roughness = 1;
+            double noiseRoughness = 1 ;
+
+            DiamondSquare IslandGenerator = new DiamondSquare(
+                new NoiseGeneratorWrapper(seed), 
+                size, 
+                roughness,
+                noiseRoughness);
+
+            double[,] data = IslandGenerator.getData() ;
+            BitmapConverter.SaveBitmap(BitmapConverter.ConvertToBitmap(data,
+                (value) =>
+                {
+                    if (value < roughness / 2)
+                    {
+                        return Color.Blue;
+                    }
+                    if (value > 1)
+                    {
+                        return Color.White;
+                    }
+                    int blackShade = Convert.ToInt32(255 * value);
+                    return Color.FromArgb(blackShade, blackShade, blackShade);
+                }), "Result.bmp");
+            
+        }
+    }
+}
